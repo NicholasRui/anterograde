e55221d73dd2171404816ccae0e96e8ffd7504cd
NicholasRui <nrui@caltech.edu>
2025-05-15 18:28:35 -0700

    Clean up implementation of main loop

--- main.cpp
+++ main.cpp
@@ -3,69 +3,61 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// initialize window
+void initializeWindow() {
+    // instantiate window
+    glfwInit();
 
+    // version number
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
+    // use core-profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+    // needed for MacOS?
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
 
-
-
+// readjust window when window size adjusted
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}  
-
-
+}
 
 int main() {
+    int res_x = 800; int res_y = 600;
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // initialize and instantiate window
+    initializeWindow();
 
-    std::cout << "hello";
+    GLFWwindow* window = glfwCreateWindow(res_x, res_y, "AnterogradeWindow", NULL, NULL);
+    if (window == NULL) { // exception if fail
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-if (window == NULL)
-{
-    std::cout << "Failed to create GLFW window" << std::endl;
+    // initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // set viewport and register dynamical adjustment when window size changes
+    glViewport(0, 0, res_x, res_y);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+    // main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // terminate and exit
     glfwTerminate();
-    return -1;
-}
-glfwMakeContextCurrent(window);
-
-
-
-if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-{
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-}    
-
-
-
-
-glViewport(0, 0, 800, 600);
-
-
-
-
-glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
-
-
-
-while(!glfwWindowShouldClose(window))
-{
-    glfwSwapBuffers(window);
-    glfwPollEvents();    
-}
-
-
-glfwTerminate();
-
-
     return 0;
-
 }
